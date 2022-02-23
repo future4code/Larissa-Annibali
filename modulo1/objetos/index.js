dcfaea7f2702fb8a453eb10bdf25309a91bf2c15
Larissa Annibali <larissa.annibali@gmail.com>
2022-02-23 02:18:42 -0300

    TraçosDeUmaPessoaComInsôniaParandoDeFuncionar.exe

--- modulo1/objetos/index.js
+++ modulo1/objetos/index.js
@@ -73,10 +73,7 @@ const pessoaEu = {
 
   }
 
-  
- 
-
- /*b) Escreva uma função que receba esses objetos e retorne um array com as seguintes informações:
+   /*b) Escreva uma função que receba esses objetos e retorne um array com as seguintes informações:
 
 1. O valor de `nome`
 2. O numero de caracteres do valor `nome`
@@ -94,3 +91,106 @@ function champions (pessoasProfissoes, pessoasProfissoes2) {
     console.log (array2)
 }
 
+//3. Resolva os passos a seguir:
+/*a) Crie uma variável de escopo global que guarde um `array` vazio chamada `carrinho`*/
+
+const carrinho = []
+
+/*b) Crie três novos objetos que representem frutas de um sacolão. E
+les devem ter as seguintes propriedades: nome (`string`) e 
+disponibilidade (`boolean` - devem começar como `true`)*/
+
+const fruta0 = {
+    nome: 'Manga',
+    disponibilidade: true
+
+}
+const fruta1 = {
+    nome: 'Pêra',
+    disponibilidade: true
+}
+const fruta2 = {
+    nome: 'Morango',
+    disponibilidade: true
+}
+
+
+/*c) Crie uma função que **receba** um objeto fruta por **parâmetro** e coloque-a 
+dentro do array de `carrinho`. Invoque essa função passando os três objetos criados.*/
+const fruta = receberFrutas => {
+    carrinho.push(receberFrutas)
+}
+
+fruta ([fruta0, fruta1, fruta2])
+/*d) Imprima a variável carrinho e garanta que ela agora seja um array preenchido com três objetos. */
+
+console.log(carrinho)
+
+//Desafios 
+
+
+/*1. Crie um função que pergunte ao usuário seu nome, sua idade e sua profissão e depois imprima no console 
+um objeto com essas propriedades. Depois de imprimir o novo objeto, imprima também o tipo dele para 
+garantir que é um objeto.*/
+
+function informacoesUsuario (nome, idade, profissao) {
+    return nome, idade, profissao}
+
+const informacoesInseridas = {
+ nome:'Larissa',
+ idade:'26',
+ profissao: 'Atendente'
+}
+
+console.log(`nome: ${informacoesInseridas.nome} idade: ${informacoesInseridas.idade} profissão: ${informacoesInseridas.profissao}.`)
+
+const informacoesImportantes = {
+   infos: 'Informações sobre o usuário'
+}
+console.log(informacoesImportantes.infos)
+
+/*Crie uma função que receba dois objetos que representam filmes. Eles devem ter as 
+propriedades: ano de lançamento e nome. A função deve retornar uma mensagem no seguinte modelo:
+O primeiro filme foi lançado antes do segundo? false
+O primeiro filme foi lançado no mesmo ano do segundo? true*/
+
+const filme = {
+	nome: 'Um Amor Para Recordar', 
+	ano: '2002', 
+    lançado1: false
+}
+
+const filme2 = {
+    nome: 'A visita',
+    ano: '2015',
+    lançado2: false
+}
+
+console.log(`O primeiro filme foi lançado antes do segundo? ${filme.lançado1}.`)
+console.log(`O primeiro filme foi lançado no mesmo ano do segundo? ${filme2.lançado2}.`)
+
+/*3. Crie uma função a mais pro exercício 3 de escrita de código. Essa função vai auxiliar o 
+controle de estoque do sacolão: ela deve receber por parâmetro uma das frutas e retornar esse 
+mesmo objeto com a propriedade disponibilidade com o valor invertido. */
+
+
+
+const fruta4 = {
+    nome: 'Banana',
+    disponibilidade: true
+}
+
+const fruta5 = {
+    nome: 'kiwi',
+    disponibilidade: false
+}
+
+const frutas = receberFrutas2 => {
+    carrinho.push(receberFrutas2)
+}
+
+frutas ([fruta4, fruta5])
+
+console.log(carrinho)
+
+//rezando a deus do sono que depois de tudo isso eu tenha acertado pelo menos alguma coisa plmdds
