5204dbecae61c9e6b5086fcf60ab12cb2392eb78
Larissa Annibali <larissa.annibali@gmail.com>
2022-03-18 23:58:57 -0300

    retorna numeros pares elevados

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -5,35 +5,41 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   return array.length
+    return array.length
 }
 
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  return array.reverse()
+    return array.reverse()
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-    return array.sort((a, b) => (a-b))
+    return array.sort((a, b) => (a - b))
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  const numeroPar = a => a % 2 === 0
-  return array.filter(numeroPar)   
+    const numeroPar = a => a % 2 === 0
+    return array.filter(numeroPar)
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
     let numerosElevados = []
-    for (let i = 0; i < array.length; i +)
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            numerosElevados.push((array[i] * array[i]))
+        }
+    }
+    return numerosElevados
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+    
+
 }
 
 // EXERCÍCIO 07
@@ -43,7 +49,7 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+
 }
 
 // EXERCÍCIO 09
@@ -53,27 +59,27 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+
 }
 
 // EXERCÍCIO 14
@@ -83,10 +89,10 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+
 }
 
 // EXERCÍCIO 15B
 function retornaArrayOrdenadoPorData(consultas) {
-   
+
 }
